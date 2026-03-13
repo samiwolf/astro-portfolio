9b3e20ca04dbd3991b0f41d8e08ea8382b8547b1
samiul.siddiqui <samiul.siddiqui@streamstech.com>
2026-03-14 01:59:21 +0600

    Remove blog module and all related components and content; make it a cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
+	integrations: [tailwind()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
